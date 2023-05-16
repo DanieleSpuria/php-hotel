1d3c3553a30bb9d5ea54c1647d6bf7261f11a92f
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-05-16 18:03:34 +0200

    add table

--- index.php
+++ index.php
@@ -38,11 +38,11 @@
   ];
 
   foreach($hotels as $key => $value) {
-    echo '<pre>';
       var_dump( $value);
-    echo '</pre>';
   }
 ?>
+
+
 
 
 <!DOCTYPE html>
@@ -51,15 +51,70 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
   <title>php-Hotel</title>
 </head>
 <body>
-  <?php 
+
+  <!-- <?php 
     foreach($hotels as $keyHotel => $hotel) {
       foreach($hotel as $keyParam => $param) {
         echo "<h1> $keyParam: $param </h1>";
       }   
     }
-  ?>
+  ?> -->
+
+  <table class='table'>
+    <thead>
+      <tr>
+        <th scope="col">Hotels</th>
+        <th scope='col'>Parking</th>
+        <th scope='col'>Vote</th>
+        <th scope='col'>Distance to center</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <?php foreach($hotels as $keyHotel => $hotel) : ?>
+
+        <tr>
+          <th scope="row"><?php echo $hotel['name']; ?></th>
+          <td><?php echo $hotel['parking'] ? 'Yes' : 'No'; ?></td>
+          <td><?php echo $hotel['vote']; ?></td>
+          <td><?php echo $hotel['distance_to_center']; ?></td>
+        </tr>
+
+      <?php endforeach; ?>
+    </tbody>
+  </table> 
+    <!-- <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table> -->
 </body>
 </html>
