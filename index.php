dd8cb0cd0e4ceda652731a4e8fd86238848421e1
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-05-16 15:26:33 +0200

    print hotels

--- index.php
+++ index.php
@@ -1,41 +1,47 @@
 <?php
   $hotels = [
     [
-        'name' => 'Hotel Belvedere',
-        'description' => 'Hotel Belvedere Descrizione',
-        'parking' => true,
-        'vote' => 4,
-        'distance_to_center' => 10.4
+      'name' => 'Hotel Belvedere',
+      'description' => 'Hotel Belvedere Descrizione',
+      'parking' => true,
+      'vote' => 4,
+      'distance_to_center' => 10.4
     ],
     [
-        'name' => 'Hotel Futuro',
-        'description' => 'Hotel Futuro Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 2
+      'name' => 'Hotel Futuro',
+      'description' => 'Hotel Futuro Descrizione',
+      'parking' => true,
+      'vote' => 2,
+      'distance_to_center' => 2
     ],
     [
-        'name' => 'Hotel Rivamare',
-        'description' => 'Hotel Rivamare Descrizione',
-        'parking' => false,
-        'vote' => 1,
-        'distance_to_center' => 1
+      'name' => 'Hotel Rivamare',
+      'description' => 'Hotel Rivamare Descrizione',
+      'parking' => false,
+      'vote' => 1,
+      'distance_to_center' => 1
     ],
     [
-        'name' => 'Hotel Bellavista',
-        'description' => 'Hotel Bellavista Descrizione',
-        'parking' => false,
-        'vote' => 5,
-        'distance_to_center' => 5.5
+      'name' => 'Hotel Bellavista',
+      'description' => 'Hotel Bellavista Descrizione',
+      'parking' => false,
+      'vote' => 5,
+      'distance_to_center' => 5.5
     ],
     [
-        'name' => 'Hotel Milano',
-        'description' => 'Hotel Milano Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 50
+      'name' => 'Hotel Milano',
+      'description' => 'Hotel Milano Descrizione',
+      'parking' => true,
+      'vote' => 2,
+      'distance_to_center' => 50
     ],
   ];
+
+  foreach($hotels as $key => $value) {
+    echo '<pre>';
+      var_dump( $value);
+    echo '</pre>';
+  }
 ?>
 
 
@@ -48,6 +54,12 @@
   <title>php-Hotel</title>
 </head>
 <body>
-  <h1>xiao</h1>
+  <?php 
+    foreach($hotels as $keyHotel => $hotel) {
+      foreach($hotel as $keyParam => $param) {
+        echo "<h1> $keyParam: $param </h1>";
+      }   
+    }
+  ?>
 </body>
 </html>
